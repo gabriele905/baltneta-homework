d321039b554d073d4e839e8d184228b6f1c420e6
gabriele <gabriele@universumas.com>
2024-08-07 21:16:17 +0300

    Add login middleware

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.middlewares.login.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
