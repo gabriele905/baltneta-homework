8930689daef0781a8463b7cf67ab4f111c6d24cc
gabriele <gabriele@universumas.com>
2024-08-07 21:19:27 +0300

    Add Category model

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+    number = models.IntegerField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    create_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+
+    def __str__(self):
+        return self.category
