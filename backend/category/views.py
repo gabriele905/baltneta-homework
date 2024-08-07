454f6baf690c85ea8a481887c450f4a8453745ee
gabriele <gabriele@universumas.com>
2024-08-07 21:26:09 +0300

    Add upload file view

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import UploadFileForm
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+
+            return HttpResponseRedirect('/categories/')
+    else:
+        form = UploadFileForm()
+
+    return render(request, 'upload.html', {'form': form})
+
+
+def handle_uploaded_file(f):
+    return
